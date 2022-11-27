3e37fb1c97858cc88b068958106657568df0cb83
Mojammel-1361 <mojammel1361@gmail.com>
2022-11-28 01:48:41 +0600

    all most done

--- index.js
+++ index.js
@@ -23,99 +23,100 @@ const client = new MongoClient(uri, {
 async function run(){
     
     try{
-        const dataCategoryCollections = client.db("marketPortal").collection("CategoryCollections");
-        const addCardsCollections = client.db("marketPortal").collection("addCards");
-        const usersCollections = client.db("marketPortal").collection("users");
-        const productsCollections = client.db("marketPortal").collection("products");
+      const dataCategoryCollections = client
+        .db("marketPortal")
+        .collection("CategoryCollections");
+      const addCardsCollections = client
+        .db("marketPortal")
+        .collection("addCards");
+      const usersCollections = client.db("marketPortal").collection("users");
+      const productsCollections = client
+        .db("marketPortal")
+        .collection("products");
 
-        app.post("/products", async (req, res) => {
-          const products = req.body;
-          const result = await productsCollections.insertOne(products);
-          res.send(result);
-        });
-        app. get("/products", async (req, res) => {
-          const query = {};
-          const products = await productsCollections.find(query).toArray();
-          res.send(products);
-        });
+      app.post("/products", async (req, res) => {
+        const products = req.body;
+        const result = await productsCollections.insertOne(products);
+        res.send(result);
+      });
+      app.get("/products", async (req, res) => {
+        const query = {};
+        const products = await productsCollections.find(query).toArray();
+        res.send(products);
+      });
 
-        app.get("/users", async (req, res) => {
-          const query = {};
-          const users = await usersCollections.find(query).toArray();
-          res.send(users);
-        });
+      app.get("/users", async (req, res) => {
+        const query = {};
+        const users = await usersCollections.find(query).toArray();
+        res.send(users);
+      });
 
-        app.delete('/users/:id', async(req, res) =>{
-          const id = req.params.id;
-          const filter = { _id: ObjectId(id) };
-          const result = await usersCollections.deleteOne(filter);
-          res.send(result);
-        });
+      app.delete("/users/:id", async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const result = await usersCollections.deleteOne(filter);
+        res.send(result);
+      });
 
-        app.post('/users', async(req, res) =>{
-            const user = req.body;
-            const result = await usersCollections.insertOne(user);
-            res.send(result);
-        })
+      app.post("/users", async (req, res) => {
+        const user = req.body;
+        const result = await usersCollections.insertOne(user);
+        res.send(result);
+      });
 
-         app.get('/users/admin/:email', async (req, res) => {
-           const email = req.params.email;
-           const query = { email };
-           const user = await usersCollections.findOne(query);
-           res.send({ isAdmin: user?.role === "Admin" });
-         });
+      app.get("/users/admin/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const user = await usersCollections.findOne(query);
+        res.send({ isAdmin: user?.role === "Admin" });
+      });
 
-        //  app.get("/users/buyer/:email", async (req, res) => {
-        //    const email = req.params.email;
-        //    const query = { email };
-        //    const user = await usersCollections.findOne(query);
-        //    res.send({ isBuyer: user?.role === "Buyer" });
-        //  });
+       app.get("/users/seller/:email", async (req, res) => {
+         const email = req.params.email;
+         const query = { email };
+         const user = await usersCollections.findOne(query);
+         res.send({ isSeller: user?.role === "Seller" });
+       });
 
+      app.post("/addCards", async (req, res) => {
+        const addCard = req.body;
+        const result = await addCardsCollections.insertOne(addCard);
+        res.send(result);
+      });
 
-        app.post('/addCards', async(req, res) =>{
-            const addCard = req.body
-            const result = await addCardsCollections.insertOne(addCard);
-            res.send(result);
-        });
+      app.get("/addCards", async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const options = await addCardsCollections.find(query).toArray();
+        res.send(options);
+      });
 
-         app.get("/addCards", async (req, res) => {
-           const email = req.query.email;
-           const query = {email: email};
-           const options = await addCardsCollections.find(query).toArray();
-           res.send(options);
-         });
+      app.get("/categoryOptions", async (req, res) => {
+        const query = {};
+        const options = await dataCategoryCollections.find(query).toArray();
+        res.send(options);
+      });
 
+      app.get("/categoryOptions/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const categoryOption = await dataCategoryCollections.findOne(query);
+        res.send(categoryOption);
+      });
 
-        app.get('/categoryOptions', async (req, res) =>{
-            const query = {};
-            const options = await dataCategoryCollections.find(query).toArray();
-            res.send(options);
-        });
+      app.get("/jwt", async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+        const user = await usersCollections.findOne(query);
+        if (user) {
+          const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+            expiresIn: "2day",
+          });
+          return res.send({ accessToken: token });
+        }
 
-
-
-        
-        app.get('/categoryOptions/:id', async (req, res) =>{
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const categoryOption = await dataCategoryCollections.findOne(query);
-            res.send(categoryOption); 
-        })
-
-        app.get("/jwt", async (req, res) => {
-          const email = req.query.email;
-          const query = { email: email };
-          const user = await usersCollections.findOne(query);
-          if (user) {
-            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
-              expiresIn: "2day",
-            });
-            return res.send({ accessToken: token });
-          }
-
-          res.status(403).send({ accessToken: "" });
-        });
+        res.status(403).send({ accessToken: "" });
+      });
     }
 
     finally{
